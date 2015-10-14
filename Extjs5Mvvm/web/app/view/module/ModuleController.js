4e2df22736ea859ebb2834e7c6b63c4c052fe677
liuyang2000 <847479481@qq.com>
2015-10-14 22:29:48 +0800

    将tabPanel 抽取为一个js类 点击菜单在tabPanel中增加tab页

---
+++ Extjs5Mvvm/web/app/view/module/ModuleController.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2015/10/14.
+ */
+/**
+ * 模块的控制器
+ */
+
+Ext.define('app.view.module.ModuleController', {
+    extend: 'Ext.app.ViewController',
+
+    //requires: ['Ext.MessageBox', 'Ext.window.Toast'],
+
+    alias: 'controller.module',
+
+    init: function () {
+        console.log('modulecontroller.init')
+    }
+
+})
---
+++ Extjs5Mvvm/web/app/view/module/region/Navigate.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Administrator on 2015/10/14.
+ */
+/**
+ * 导航区域的主控界面,这是这个系统的核心控件之一
+ */
+
+Ext.define('app.view.module.region.Navigate', {
+    extend : 'Ext.panel.Panel',
+    alias : 'widget.navigate',
+
+    // glyph : 0xf0d0,
+    title : '导航',
+
+    initComponent : function() {
+
+        this.callParent();
+    }
+
+})
