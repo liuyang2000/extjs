6c146ab351e3fa1359a596641d9afa7b980507c8
liuyang2000 <847479481@qq.com>
2015-10-14 22:57:28 +0800

    model和columns生成好了，下面要修改一下Module.js和Grid.js中的代码，使其能够协同工作 给新增按钮增加一个事件执行， handler : 'addRecord',

--- Extjs5Mvvm/web/app/view/module/Module.js
+++ Extjs5Mvvm/web/app/view/module/Module.js
@@ -9,10 +9,13 @@ Ext.define('app.view.module.Module', {
 
     alias: 'widget.modulepanel',
 
-    requires: ['app.view.module.ModuleController', 'app.view.module.ModuleModel'],
+    requires: ['app.view.module.ModuleController',
+                'app.view.module.ModuleModel',
+                'app.view.module.factory.ModelFactory'],
 
-    uses: ['app.view.module.region.Navigate', 'app.view.module.region.Grid',
-        'app.view.module.region.Detail'],
+    uses: ['app.view.module.region.Navigate',
+            'app.view.module.region.Grid',
+            'app.view.module.region.Detail'],
 
     controller: 'module',
     // MVVM架构的控制器的名称，main控制器会自动加载，这个控制器不会自动加载，需要在requires中指定，不知道是为什么
@@ -27,6 +30,18 @@ Ext.define('app.view.module.Module', {
 
     initComponent: function () {
         this.glyph = this.getViewModel().get('tf_glyph'); // 由于上面的glyph的bind无效，因此需要在这里加入glyph的设置
+        this.model = app.view.module.factory.ModelFactory.getModelByModule(this.getViewModel());
+        console.log(this.model);
+
+        this.store = Ext.create('Ext.data.Store', {
+            model : this.model,
+            autoLoad : true,
+            proxy : {
+                type : 'localstorage',
+                id : 'module' + '__' + this.getViewModel().get('tf_moduleName')
+            }
+        }),
+
         this.items = [{
             xtype: 'navigate', // 导航区域
             region: 'west',
@@ -35,8 +50,8 @@ Ext.define('app.view.module.Module', {
             split: true
         }, {
             xtype: 'modulegrid', // 模块的grid显示区域
-            region: 'center'
-
+            region: 'center',
+            store : this.store
         }, {
             xtype: 'recorddetail', // 记录明细
             region: 'east',
@@ -45,7 +60,7 @@ Ext.define('app.view.module.Module', {
             collapseMode: 'mini', // 折叠陷藏模式
             split: true
             // 可以拖动大小
-        }]
+        }],
 
         this.callParent();
     }
